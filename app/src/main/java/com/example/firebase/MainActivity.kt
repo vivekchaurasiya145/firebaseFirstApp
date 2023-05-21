9bad644b9da5b164a93a272ee543633bc582a563
Vivek Chaurasiya <cvivek9933@gmail.com>
2023-05-21 13:29:20 +0530

    first commit

---
+++ app/src/main/java/com/example/firebase/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.firebase
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var btn = findViewById<Button>(R.id.btn)
+
+btn.setOnClickListener {
+
+   var a=0
+    var b= 1
+var c = b/a
+
+
+}
+
+    }
+}
